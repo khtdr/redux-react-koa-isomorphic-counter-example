6bdf9b1b0a5e791a6db4d08f136128f57345371a
Kay O <khtdr.com@gmail.com>
2015-06-27 16:51:45 +0000

    show source maps for now

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+  devtool: 'source-map',
   entry: './src/client/app',
   output: {
     path: path.join(__dirname, '/public/js'),
