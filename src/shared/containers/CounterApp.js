ed8b84b3653f720061a863436cfd16c4f35c552d
Kay O <khtdr.com@gmail.com>
2015-06-27 22:25:43 +0000

    cosmetic changes

--- src/shared/containers/CounterApp.js
+++ src/shared/containers/CounterApp.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'redux/react';
 import Counter from '../components/Counter';
-import * as CounterActions from '../actions/counter';
+import * as actions from '../actions/counter';
 
 @connect(state => ({
   counter: state.counter
@@ -10,9 +10,7 @@ import * as CounterActions from '../actions/counter';
 export default class CounterApp {
   render() {
     const { counter, dispatch } = this.props;
-    return (
-      <Counter counter={counter}
-               {...bindActionCreators(CounterActions, dispatch)} />
-    );
+    const creators = bindActionCreators(actions, dispatch);
+    return <Counter {...this.props} {...creators} />;
   }
 }
